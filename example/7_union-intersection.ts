22ef06199b5eaccc47404527ada7703b01d1232c
Um seoyoung <tjdud0123@naver.com>
2022-03-11 16:46:22 +0900

    union 타입

--- example/7_union-intersection.ts
+++ example/7_union-intersection.ts
@@ -1,17 +1,17 @@
-function logMessage(value: string) {
-  console.log(value);
-}
-function logMessage(value: number) {
-  console.log(value);
-}
-function logMessage(value: any) {
-  console.log(value);
-}
+// function logMessage(value: string) {
+//   console.log(value);
+// }
+// function logMessage(value: number) {
+//   console.log(value);
+// }
+// function logMessage(value: any) {
+//   console.log(value);
+// }
 
-// # Union 타입 문법 - `any` 보다는 명시적임
-function logMessage(value: string | number) {
-  console.log(value);
-}
+// # NOTE: Union 타입 문법 - `any` 보다는 명시적임 : 타입가드시 메소드 등장
+// function logMessage(value: string | number) {
+//   console.log(value);
+// }
 
 function logMessage(value: string | number) {
   if (typeof value === 'string') {
@@ -34,7 +34,20 @@ interface Person {
   age: number;
 }
 
+// NOTE: Union 타입 => 어떤 것이 들어올지 모르므로 공통된 속성만 접근가능
 function askSomeone(someone: Developer | Person) {
   someone.name; // O
-  someone.age; // X
+  // someone.age; // X
 }
+
+askSomeone({ name: '서영', skill: 'js' });
+askSomeone({ name: '서영', age: 20 });
+
+// NOTE: Intersection 타입 => 조합된 모든 속성만 접근가능
+function askSomeone2(someone: Developer & Person) {
+  someone.name; // O
+  someone.age; // O
+  someone.skill; // O
+}
+
+askSomeone2({ name: '서영', age: 20, skill: 'js' });
