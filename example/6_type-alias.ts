fc921a2dd84dcabe996fce0c8bc5f010fc0d1652
Um seoyoung <tjdud0123@naver.com>
2022-03-11 16:45:48 +0900

    타입별칭

--- example/6_type-alias.ts
+++ example/6_type-alias.ts
@@ -1,6 +1,12 @@
 // https://www.typescriptlang.org/docs/handbook/advanced-types.html
 // https://www.typescriptlang.org/docs/handbook/advanced-types.html#nullable-types
 
+/** NOTE: 타입 별칭과 인터페이스의 차이점
+ * 타입의 확장 가능 / 불가능 여부
+ * 인터페이스는 확장이 가능한데 반해 타입 별칭은 확장이 불가능
+ * 따라서, 가능한한 type 보다는 interface로 선언해서 사용하는 것을 추천
+ */
+
 // #1
 // function sum(a: number, b:number) {
 //   return a + b;
@@ -12,13 +18,17 @@ function sum(a: SumParameter, b: SumParameter) {
 }
 
 // #2
-type Person = {
+type Student = {
   name: string;
   age: number;
 };
 
-function getPerson(): Person {
+function getPerson(): Student {
   // ...
+  return {
+    name: 'seoYoung',
+    age: 28
+  }
 }
 
 // #3
@@ -26,6 +36,6 @@ type Hero = {
   skill: string;
 }
 
-const capt: Hero = { 
-  // skill: 'throwing a shield' 
+const captain: Hero = { 
+  skill: 'throwing a shield' 
 }
