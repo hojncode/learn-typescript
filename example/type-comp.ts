a2b2a2da78986caba531426ff54bb984c75841c3
Um seoyoung <tjdud0123@naver.com>
2022-03-12 00:13:41 +0900

    타입호환

--- example/type-comp.ts
+++ example/type-comp.ts
@@ -1,30 +1,47 @@
 // 인터페이스
-interface Developer {
+interface IDeveloper {
   name: string;
   skill: string;
 }
 
-interface Person {
+interface IPerson {
   name: string;
 }
 
-var a: Developer;
-var b: Person;
-// a = b; // X
-b = a; // O
+//NOTE: 타입호환 - 작은타입에 큰타입을 넣는 것은 가능
+var developer: IDeveloper;
+var person: IPerson;
+// developer = person; // X
+person = developer; // O
 
 // 함수
-var add = function(a: number) {
+var _add = function(a: number) {
   // ...
 }
-var sum = function(a: number, b: number) {
+var _sum = function(a: number, b: number) {
   // ...
 }
-// sum = add; // X
-// add = sum; // O
+_sum = _add; // O
+// _add = _sum; // X
 
-// // 유니온 타입
-// var c: Developer | Person;
-// var d: Person | string;
-// c = d;
-// d = c;
+// 유니온 타입
+var c: IDeveloper | IPerson;
+var d: IPerson | string;
+// c = d; // X
+d = c; // O
+
+// 제네릭
+interface Empty<T> {
+}
+let x: Empty<number>;
+let y: Empty<string>;
+
+x = y;  // OK, because y matches structure of x
+
+interface NotEmpty<T> {
+  data: T;
+}
+let x1: NotEmpty<number>;
+let y1: NotEmpty<string>;
+
+// x1 = y1;  // Error, because x and y are not compatible
